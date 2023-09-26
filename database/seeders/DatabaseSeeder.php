8435353fceb5df85bccf509732e659152b67587c
Andres Santiago <andresm17u7@hotmail.com>
2023-09-26 16:38:38 -0500

    Update Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,25 +15,25 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        $this->call(AppointmentLevelsSeeder::class);
+        // $this->call(AppointmentLevelsSeeder::class);
 
-        $this->call(OfficeSeeder::class);
+        // $this->call(OfficeSeeder::class);
 
-        $this->call(PermissionSeeder::class);
+        // $this->call(PermissionSeeder::class);
 
-        $this->call(PoliticalDivisionSeeder::class);
+        // $this->call(PoliticalDivisionSeeder::class);
 
-        $this->call(PastoralSeeder::class);
+        // $this->call(PastoralSeeder::class);
 
-        $this->call(ZoneSeeder::class);
+        // $this->call(ZoneSeeder::class);
 
-        $this->call(CommunitySeeder::class);
+        // $this->call(CommunitySeeder::class);
 
-        $this->call(RoleSeeder::class);
+        // $this->call(RoleSeeder::class);
 
-        $this->call(UserSeeder::class);
+        // $this->call(UserSeeder::class);
 
-        $this->call(EventSeeder::class);
+        // $this->call(EventSeeder::class);
 
         // $this->call(ProfileSeeder::class);
 
